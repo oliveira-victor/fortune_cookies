de89503b19299c7e7f47332024c16983de72a261
victor <victor.freire.oliveira@gmail.com>
2023-02-10 13:08:03 -0300

    add: new phrases to string

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,24 @@
 let paperMsg = [
-    "You are incredible.",
-    "Today, try to take care of yourself, so you'll be able to take care of the others.",
-    "Have a nice day!",
-    "Keep calm.",
-    "Tomorrow is another day.",
-    "Better late than never.",
-    "Love is on the way.",
-    "Love is the answer.",
-    "You are beautiful."
+    "If you're waiting for a sign, this is it.",
+    "It's a good day to make that decision.",
+    "You shall receive back all the love you give away.",
+    "You will reach your goals with a little more patience.",
+    "You carry a unique beauty not ever seen in the universe.",
+    "Do you compliment yourself as much as you criticize?",
+    "The answer is love.",
+    "Don't ever regret the love you gave to someone who didn't know what to do with it.",
+    "You will meet great people as long as you have your eyes and heart open.",
+    "Life will change for better if you remember to smile every day.",
+    "It's time to take the first step for that big goal you have.",
+    "Courage isn't the absence of fear, but the capacity to keep going despite of it.",
+    "Don't do to the others what you wouldn't like that people did to you.",
+    "Stop complaining often and your life shall change for better.",
+    "It's a good time to send a nice message to that special person.",
+    "Have you said 'I love you' to someone recently?",
+    "Try to find the bright side of the things that happen to you.",
+    "Care for your peace of mind, it's the basis for a good life.",
+    "Life is for those who can adapt better. Improve that in yourself.",
+    "Be aware of the present moment you're in."
 ]
 let newMsg = ''
 let list = document.querySelector('.list')
